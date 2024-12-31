f5d5704b143c2f0ec97ffe0bf0d3d13dd8e023f7
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2024-12-31 21:25:32 +0200

    update

--- assignment 1ST JS/main.js
+++ assignment 1ST JS/main.js
@@ -1,4 +1,38 @@
+//************************************************** */
+// IN LECTURE
+//************************************************** */
 
+
+// let username = prompt("enter ur name"); 
+// let UserAge = prompt("enter ur years") 
+
+// let age = new Date().getFullYear() - UserAge;
+
+// console.log("userName: " + username + "\nAge: " + age);
+
+// or
+
+// let UserBrithy = prompt("enter ur age");
+
+// let Age = 2024 - UserBrithy;
+
+// console.log("your age is :" + Age);
+// document.write("your age is :" + Age);
+
+
+// let shirt = prompt("enter price");
+
+// let discount = 20/100;
+
+// let dicountPrice = (shirt * discount) ;
+
+// console.log("ur price:" + dicountPrice);
+
+//******************************************************************************************************* */
+
+//******************************************* */
+// THE ASSIGNMENT
+//******************************************* */
 let price = prompt("enter price");
 let discount = prompt("enter discount");
 
