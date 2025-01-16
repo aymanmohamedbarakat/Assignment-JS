758c1e0d135e700dd854cc453e90e526485edb40
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-01-16 15:48:00 +0200

    update

--- assignment 2nd JS/1/main.js
+++ assignment 2nd JS/1/main.js
@@ -1,30 +1,27 @@
 let userDegree = prompt("Please enter your degree");
 
-if (userDegree >= 0 ) {
-    if (userDegree <= 100) {
-        if (userDegree < 50 ){
-            document.write("انت راسب");
-        }else{
-            if( userDegree < 75){
-                document.write("مقبول");
-            }else{
-                if (userDegree < 85){
-                    document.write("جيد");
-                } else {
-                    if (userDegree < 90){
-                        document.write(" جيد جيدا");
-                    }else{
-                        document.write("ممتاز");
-                    }
-                }
-            }
+if (userDegree > 0) {
+  if (userDegree >= 50) {
+    if (userDegree >= 60) {
+      if (userDegree >= 80) {
+        if (userDegree >= 90) {
+          if (userDegree <= 100) {
+            document.write("ممتاز");
+          } else {
+            document.write("invaild number");
+          }
+        } else {
+          document.write("جيد جدا ");
         }
+      } else {
+        document.write("جيد ");
+      }
+    } else {
+      document.write("مقبول");
     }
-    else {
-        document.write("عذرا هذا الرقم غير صحيح");
-    }
-
+  } else {
+    document.write("faild");
+  }
 } else {
-    document.write("عذرا هذا الرقم غير صحيح");
+  document.write("invaild number");
 }
-
---
+++ assignment 3rd JS/main.js
@@ -0,0 +1,6 @@
+// calculator with eval function
+function calc(){
+  let num = prompt("enter number u need to calc")
+  let calc = eval(num);
+  document.write(calc);
+}
