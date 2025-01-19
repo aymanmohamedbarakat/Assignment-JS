d851ee4aeb6b763850c17c17afa0e10f3622fa53
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-01-19 03:16:52 +0200

    Task-commit

--- assignment 3rd JS/main.js
+++ assignment 3rd JS/main.js
@@ -1,6 +1,17 @@
 // calculator with eval function
-function calc(){
-  let num = prompt("enter number u need to calc")
-  let calc = eval(num);
-  document.write(calc);
+// function calc(){
+//   let num = prompt("enter number u need to calc")
+//   let calc = eval(num);
+//   document.write(calc);
+// }
+
+
+let userEcpresiion = "";
+
+function addString(val) {
+  userEcpresiion= userEcpresiion + val
+}
+
+function calcExp() {
+  console.log(eval(userEcpresiion))
 }
---
+++ assignment 4th JS/main.js
@@ -0,0 +1,39 @@
+let studentDegreeInput = document.querySelector("#studentDegree");
+const submitBtn = document.querySelector('.btn');
+const resultShow = document.querySelector('.result');
+
+
+let calcDegree = () => {
+  let Degree = studentDegreeInput.value;
+  let GPA = '';
+
+  if (Degree < 0 || Degree > 100 || isNaN(Degree)) {
+    resultShow.textContent = "This number not invaild";
+    return;
+  }
+
+  Degree = +(Degree);
+
+    if (Degree >= 90 ){
+      GPA = 'A (Excellent)'
+      resultShow.style.color = '#198754';  // Bootstrap success color
+    } else if (Degree >= 75){
+      GPA = 'B (Very Good)'
+      resultShow.style.color = '#0d6efd'; 
+    } else if (Degree >= 60){
+      GPA = 'C (Good)'
+      resultShow.style.color = '#6c757d'; 
+    } else if ( Degree >= 50){
+      GPA = 'D (Pass)';
+      resultShow.style.color = '#ffc107'; 
+    } else {
+      GPA =  'F (Fail)';
+      resultShow.style.color = '#dc3545';
+    }
+      resultShow.textContent = `Your GPA is: ${GPA}`;
+      studentDegreeInput.value = '';
+  }
+
+  // من أجل الشياكة 
+
+ 
